b3945fe75e666b3abad470449b4777885f1555a3
Bjoern Griebenow <b.griebenow@web.de>
2020-07-20 18:09:06 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,210 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+
+use std::collections::{HashMap, HashSet};
+
+use std::{fs, io};
+use std::path::Path;
+
+use std::{ffi::OsString, process::Command};
+
+use regex::Regex;
+
+use rocket::Request;
+// use rocket::response::content;
+use rocket::response::content::Content;
+use rocket::http::ContentType;
+// use rocket::response::NamedFile;
+// use rocket::response::{self, Response, Responder};
+
+// use rocket_contrib::Template;
+use rocket_contrib::templates::Template;
+// use handlebars::Handlebars;
+// use handlebars::Context;
+
+use tera::Context;
+
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate lazy_static;
+
+
+// 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 426, 428, 429, 431, 451, 500, 501, 503, and 510
+
+
+lazy_static! {
+    static ref DATA_SOURCE_BASE_PATH: String = {
+        let data_source_base_path = fs::read_dir("/var/lib/collectd/rrd/")
+            .unwrap()
+            .next()
+            .unwrap()
+            .unwrap()
+            .path()
+            .to_str()
+            .unwrap()
+            .to_string();
+
+        let dbg_msg = String::from("Data source directory is: ") + &data_source_base_path;
+        dbg!(dbg_msg);
+        data_source_base_path
+    };
+
+    // static ref PRIVILEGES: HashMap<&'static str, Vec<&'static str>> = {
+    //     let mut map = HashMap::new();
+    //     map.insert("James", vec!["user", "admin"]);
+    //     map.insert("Jim", vec!["user"]);
+    //     map
+    // };
+}
+
+
+#[catch(500)]
+fn internal_error() -> &'static str {
+    "Whoops! Looks like we messed up."
+}
+
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("I couldn't find '{}'. Try something else?", req.uri())
+}
+
+
+fn available_rrd_data_sources() -> std::vec::Vec<String> {
+    fs::read_dir(&*DATA_SOURCE_BASE_PATH).unwrap()
+        .map(
+            |res| res.map(
+                |e|
+                e.file_name().into_string().unwrap()
+        ))
+        .collect::<Result<Vec<_>, io::Error>>().unwrap()
+}
+
+
+fn rrd_files_in_data_source(data_source_name: &str) -> std::vec::Vec<String> {
+    let data_source_path = Path::new(&*DATA_SOURCE_BASE_PATH).join(data_source_name);
+    fs::read_dir(data_source_path).unwrap()
+        .map(
+            |res| res.map(
+                |e|
+                e.path().to_str().unwrap().to_string()
+                // e.file_name().into_string().unwrap()
+        ))
+        .collect::<Result<Vec<_>, io::Error>>().unwrap()
+}
+
+
+fn rrdgraph_command(args: Vec<&str>) -> Vec<u8> {
+    // let command =
+        Command::new("rrdtool")
+        .arg("graph")
+        .arg("-")              // write to stdout
+        .arg("--start").arg("-86400")
+        .arg("-u").arg("100")
+        .arg("-l").arg("0")
+        .arg("-w").arg("1000") // width
+        .arg("-h").arg("400")  // height
+        .arg("-r")
+        .args(args)
+        .output().expect("").stdout
+}
+
+
+#[get("/graph/cpu-0")]
+// #[get("/")]
+fn graph_cpu_0() ->  rocket::response::Content<std::vec::Vec<u8>> {
+    let output = rrdgraph_command(vec![
+        &["DEF:user=",   &*DATA_SOURCE_BASE_PATH, "/cpu-0/cpu-user.rrd:value:AVERAGE"].concat(),   "AREA:user#00ff00:\"Benutzer\"",
+        &["DEF:nice=",   &*DATA_SOURCE_BASE_PATH, "/cpu-0/cpu-nice.rrd:value:AVERAGE"].concat(),   "AREA:nice#00ffff:\"Prioritaet\":STACK",
+        &["DEF:system=", &*DATA_SOURCE_BASE_PATH, "/cpu-0/cpu-system.rrd:value:AVERAGE"].concat(), "AREA:system#ff0000:\"System\":STACK",
+        &["DEF:iowait=", &*DATA_SOURCE_BASE_PATH, "/cpu-0/cpu-wait.rrd:value:AVERAGE"].concat(),   "AREA:iowait#0000ff:\"IO-Wait\":STACK"
+    ]);
+    let response = Content(ContentType::PNG, output);
+    response
+}
+
+
+#[get("/graph/<data_source_name>/<rrd_filename>", rank = 2)]
+// #[get("/")]
+fn graph(
+    data_source_name: String,
+    rrd_filename:     String
+) -> rocket::response::Content<std::vec::Vec<u8>>
+{
+    // let rrd_file = &rrd_files_in_data_source(&data_source_name)[0];
+    let rrd_abs_filesystem_filename = String::from(&*DATA_SOURCE_BASE_PATH) + "/" + &data_source_name + "/" + &rrd_filename;
+    let output = rrdgraph_command(vec![
+        &["DEF:user=", &rrd_abs_filesystem_filename, ":value:AVERAGE"].concat(),     &["AREA:user#00ff00:\"", "test", "\""].concat()
+    ]);
+    let response = Content(ContentType::PNG, output);
+    response
+}
+
+
+#[get("/all_graphs")]
+// fn all_graphs() -> &'static str {
+fn all_graphs() -> Template {
+
+    // let mut context = tera::Context::new();
+    // context.insert("data_sources_with_files",
+    //                available_rrd_data_sources()
+    //                .iter()
+    //                .map(|a| {
+    //                    // let rrd_files: HashSet<_> = rrd_files_in_data_source(&a)
+    //                    //     .iter()
+    //                    //     .map(|abs_file| abs_file)
+    //                    //     .collect();
+    //                    let rrd_files = rrd_files_in_data_source(&a);
+    //                    (a, rrd_files)
+    //                })
+    //                .collect());
+
+    let abs_path_part = String::from(&*DATA_SOURCE_BASE_PATH) + "/";
+    let data_sources_with_files: HashMap<_, _> =
+        available_rrd_data_sources()
+        .into_iter()
+        .map(|a| {
+            let rrd_files: HashSet<_> = rrd_files_in_data_source(&a)
+                .into_iter()
+                .map(|abs_file| abs_file.replacen(&abs_path_part, "", 1))
+                .collect();
+            // let rrd_files = rrd_files_in_data_source(&a);
+            (a, rrd_files)
+        })
+        .collect();
+
+    let mut context = tera::Context::new();
+
+    // dbg!(data_sources_with_files);
+    context.insert("data_sources_with_files", &data_sources_with_files);
+
+    let temp = Template::render("all_graphs", &context);
+    temp
+}
+
+
+#[get("/")]
+fn index() -> &'static str {
+    "Mein Schwein ist das beste!"
+}
+
+
+fn main() {
+    // rocket::ignite().mount("/", routes![index]).launch();
+    // rocket::ignite().register(catchers![internal_error, not_found]);
+    // rocket::ignite().mount("/",            routes![graph]).launch();
+    // rocket::ignite().mount("/",            routes![index]).launch();
+    // rocket::ignite().mount("/",            routes![index]).launch();
+
+    dbg!(&*DATA_SOURCE_BASE_PATH);
+
+    rocket::ignite()
+        .mount("/",  routes![
+            graph_cpu_0,
+            graph,
+            all_graphs,
+            index
+        ])
+        .attach(Template::fairing())
+        .launch();
+}
