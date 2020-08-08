7157159c996a317ff3f3a85ca2526264391d763d
Bjoern Griebenow <b.griebenow@web.de>
2020-08-09 00:11:17 +0200

    refactor: use string constant for collectd base path

--- src/main.rs
+++ src/main.rs
@@ -32,15 +32,17 @@ use rocket_include_static_resources::StaticResponse;
 // 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 426, 428, 429, 431, 451, 500, 501, 503, and 510
 
 
+const COLLECTD_RRD_DATA_BASE_PATH: &str = "/var/lib/collectd/rrd/";
+
+
 fn first_machine_path_in_collectd_path() -> Option<String> {
-    let data_source_base_path = fs::read_dir("/var/lib/collectd/rrd/").ok()?
+    let data_source_base_path = fs::read_dir(COLLECTD_RRD_DATA_BASE_PATH).ok()?
         .next()?.ok()?
         .path()
         .to_str()?
         .to_string();
 
     Some(data_source_base_path)
-
 }
 
 
